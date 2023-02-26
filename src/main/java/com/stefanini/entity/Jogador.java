bffd758ac19101f036b2442d6994d43aff0bc8c7
SamuelGSouza <samuelgomes318@gmail.com>
2023-02-26 15:43:55 -0300

    refactor: adicionando getters, setters e construtor na entidade jogador

--- src/main/java/com/stefanini/entity/Jogador.java
+++ src/main/java/com/stefanini/entity/Jogador.java
@@ -1,5 +1,7 @@
 package com.stefanini.entity;
 
+import com.stefanini.dto.JogadorDTO;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -26,10 +28,66 @@ public class Jogador {
 
     @ManyToMany
     @JoinTable(name = "Jogador_Stefamon",
-            joinColumns = {@JoinColumn(name = "IdJogador")},
-            inverseJoinColumns = {@JoinColumn(name = "IdStefamon")})
+            joinColumns = {@JoinColumn(name = "id_jogador")},
+            inverseJoinColumns = {@JoinColumn(name = "id_stefamon")})
     private List<Stefamon> stefamons = new ArrayList<>();
 
     public Jogador() {
+    }
+
+    public Jogador(JogadorDTO jogadorDTO) {
+        id = jogadorDTO.getId();
+        nickname = jogadorDTO.getNickname();
+        password = jogadorDTO.getPassword();
+        saldo = jogadorDTO.getSaldo();
+        stefamons = jogadorDTO.getStefamons();
+    }
+
+    public Jogador(Long id, String nickname, String password, BigDecimal saldo, List<Stefamon> stefamons) {
+        this.id = id;
+        this.nickname = nickname;
+        this.password = password;
+        this.saldo = saldo;
+        this.stefamons = stefamons;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Stefamon> getStefamons() {
+        return stefamons;
+    }
+
+    public void setStefamons(List<Stefamon> stefamons) {
+        this.stefamons = stefamons;
     }
 }
