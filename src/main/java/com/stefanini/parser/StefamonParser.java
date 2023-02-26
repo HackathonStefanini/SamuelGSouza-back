b27b8644b0e1cb9bcd1fa5c158e3a5b31e5d202d
SamuelGSouza <samuelgomes318@gmail.com>
2023-02-26 15:46:08 -0300

    fix: corrige parser de Stefamon

--- src/main/java/com/stefanini/parser/StefamonParser.java
+++ src/main/java/com/stefanini/parser/StefamonParser.java
@@ -1,13 +1,17 @@
 package com.stefanini.parser;
 
+import com.stefanini.dto.JogadorDTO;
 import com.stefanini.dto.StefamonDTO;
+import com.stefanini.entity.Jogador;
 import com.stefanini.entity.Stefamon;
 
 public class StefamonParser {
 
-    public static Stefamon DtotoEntity(StefamonDTO dto) {
-        Stefamon stefamon = new Stefamon();
-        return stefamon;
+    public static StefamonDTO stefamonEntity(Stefamon entity) {
+        return new StefamonDTO(entity.getId(),
+                entity.getNome(), entity.getVida(),
+                entity.getAtaque(), entity.getDefesa(),
+                entity.getInteligencia(), entity.getPoder(),
+                entity.getVelocidade(), entity.getUrlFoto());
     }
-
 }
