ff13047676fe7498a3515e65c80a209e19fec452
SamuelGSouza <samuelgomes318@gmail.com>
2023-02-26 15:50:37 -0300

    refactor: adiciona @Produces

--- src/main/java/com/stefanini/resources/StefamonResource.java
+++ src/main/java/com/stefanini/resources/StefamonResource.java
@@ -6,6 +6,8 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/stefamon")
@@ -16,6 +18,7 @@ public class StefamonResource {
 
     @GET
     @Path("/todos")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response listarTodos() {
         return Response.status(Response.Status.OK).entity(service.listarTodos()).build();
     }
