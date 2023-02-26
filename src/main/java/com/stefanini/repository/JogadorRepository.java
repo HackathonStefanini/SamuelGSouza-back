7e568e94d71bcc02fb1b668cd4ac791c629f950d
SamuelGSouza <samuelgomes318@gmail.com>
2023-02-26 15:54:58 -0300

    feat: adiciona consulta de login

--- src/main/java/com/stefanini/repository/JogadorRepository.java
+++ src/main/java/com/stefanini/repository/JogadorRepository.java
@@ -2,9 +2,24 @@ package com.stefanini.repository;
 
 import com.stefanini.dao.GenericDAO;
 import com.stefanini.entity.Jogador;
+import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
 @RequestScoped
 public class JogadorRepository extends GenericDAO<Jogador, Long> {
+    public Jogador verificarCredenciais(@QueryParam("nickname") String nickname, @QueryParam("password") String password) {
+        try {
+            return getEntityManager().createQuery(
+                            "SELECT j FROM Jogador j WHERE j.nickname = :nickname AND j.password = :password",
+                            Jogador.class
+                    ).setParameter("nickname", nickname)
+                    .setParameter("password", password)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 }
